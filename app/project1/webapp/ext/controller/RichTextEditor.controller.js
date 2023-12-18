be9f7676a7075b06e4ee64e2953500d936fb1a16
Lukas Hannawald <lukas.hannawald@msg.group>
2023-12-18 14:02:42 +0100

    test2

--- app/project1/webapp/ext/controller/RichTextEditor.controller.js
+++ app/project1/webapp/ext/controller/RichTextEditor.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/ControllerExtension",
-	"sap/m/MessageToast"
-], function (ControllerExtension, MessageToast) {
+	"sap/ui/richtexteditor/RichTextEditor"
+], function (ControllerExtension,
+	RichTextEditor) {
 	'use strict';
 
 	return ControllerExtension.extend('project1.ext.controller.RichTextEditor', {
@@ -12,10 +13,14 @@ sap.ui.define([
              * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
              * @memberOf project1.ext.controller.RichTextEditor
              */
-			onInit: function () {
+			onInit: function (oObj) {
 				// you can access the Fiori elements extensionAPI via this.base.getExtensionAPI
-				// var oModel = this.base.getExtensionAPI().getModel();
-				this.byId("idVerticalLayout")
+				var oExtentionAPI = this.base.getExtensionAPI();
+				var oRTE = oExtentionAPI.byId("fe::CustomSubSection::RichTextEditor--idVerticalLayout");
+				//var oRTE =  oExtentionAPI.extension.project1.ext.controller.RichTextEditor.getView();
+				oRTE.addContent(new RichTextEditor());
+
+				this.byId("idVerticalLayout");
 			}
 		}
 	});
